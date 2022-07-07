3b264b809225e401ce2357104832af1f17ddc3f3
ArcadeDan <lordaniel24@gmail.com>
2022-07-07 17:16:04 -0500

    init

--- linux/main.cpp
+++ linux/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+
+#include <string>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <GL/gl.h>
+#include <GL/glx.h>
+
+
 int main(int argc, char** argv) {
     std::cout << "Hello, World" << std::endl;
     return 1;

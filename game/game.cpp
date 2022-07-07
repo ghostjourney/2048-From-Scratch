345e0bbbb4225e4a0e77ef9d421386bf5800cc91
ghostpepper <ghostsjourney@gmail.com>
2022-07-07 19:40:20 -0400

    Added game files and library

---
+++ game/game.cpp
@@ -0,0 +1,4 @@
+#include "game.hpp"
+Game2048::Game2048::Game2048() {
+};
+
---
+++ game/game.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Game2048 {
+    class Game2048 {
+        public:
+        Game2048();
+
+    };
+};

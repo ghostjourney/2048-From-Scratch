17c847da2cce6d14c78e68919701cae3b571c2a0
ghostpepper <ghostsjourney@gmail.com>
2022-07-09 13:31:58 -0400

    started adding general purpose support for shaders

---
+++ library/shader_library.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+
+class Shader;
+
+/**
+ * holds all shaders and programs
+ *
+ * do to the natures of opengl and metal, the shader storage and use is platform specific. 
+ * Therefore platform specific versions of this class must be created.
+ *
+ */
+class ShaderLibrary {
+
+    public:
+
+    /**
+     * retrieves a shader object
+     * 
+     * \param name name of shader
+     */
+    virtual Shader* GetShader(std::string name)=0;
+
+    private:
+
+};

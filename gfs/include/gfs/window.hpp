13d4bf0564da7d3abaae72e602f713d7f6b6bae3
ghostpepper <ghostsjourney@gmail.com>
2022-07-10 18:05:41 -0400

    Added some documentation to window class

--- gfs/include/gfs/window.hpp
+++ gfs/include/gfs/window.hpp
@@ -6,18 +6,51 @@
 
 namespace gfs {
 
+/**
+ * Abstract Window handler
+ *
+ * Holds and controls the window for the game. Platform specific subclasses provided to application
+ * based on this class
+ */
 class Window {
     public:
+    Window()=default;
+    /** window should never be copied */
+    Window(const Window&)=delete;
+
+    /** window should never be copied */
+    Window& operator=(const Window&)=delete;
+
+    /**
+     * First call after the constructor. Initializes the Window. 
+     * \returns true if successful. Otherwise false.
+     */
     virtual bool Init(void)=0;
-    virtual void SetTitle(std::string title);
     
+    /**
+     * Sets the window title. Should be override to set the underlying window title
+     * \param title new title
+     */ 
+    virtual void SetTitle(std::string title);
+   
+    /**
+     * Sets the height and width of the window. Should be override with platform specific handling.
+     * \param height new height
+     * \param width new width
+     */
     virtual void SetHeightWidth(double height, double width) { mWidth = width; mHeight = height; }
 
     virtual void Draw(gfs::Buffer<float, 2>& vertices)=0;
 
     private:
+
+    /// locally cached window title
     std::string mTitle;
+
+    /// locally cached window width
     double mWidth;
+
+    /// locally cached window height
     double mHeight;
 };
 };
